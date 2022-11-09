ba89a7d019693bd3ae77b5c621828ef7537d5ef0
osmomysl <dannvx@gmail.com>
2022-11-09 14:24:06 +0200

    fix migrate-mongo config

--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -16,7 +16,7 @@ const config = {
   },
 
   // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
-  migrationsDir: "store/migrations",
+  migrationsDir: "bazar-store/migrations",
 
   // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
   changelogCollectionName: "changelog",
