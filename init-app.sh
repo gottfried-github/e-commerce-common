974014389777b527cb35c650f14618e7c9d04a24
osmomysl <dannvx@gmail.com>
2023-07-04 23:42:29 +0300

    init-app script: build e-commerce-app front-end

--- init-app.sh
+++ init-app.sh
@@ -4,4 +4,9 @@
 /app/e-commerce-common/node_modules/.bin/migrate-mongo up -f /app/e-commerce-common/migrate-mongo-config.js
 
 # create admin user for the application
-node /app/e-commerce-signup/src/cli.js $USER_NAME $USER_EMAIL $USER_PSSWD
+node --trace-warnings /app/e-commerce-signup/src/cli.js $USER_NAME $USER_EMAIL $USER_PSSWD
+
+cd /app/e-commerce-app/
+npm run build
+
+cd /
